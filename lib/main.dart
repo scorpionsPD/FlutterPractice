603a983eb0b17563b2bbec18b81e5f2a6be980ec
Pradeep Dahiya <pradeep.dahiya@crmnext.com>
2021-12-29 16:17:55 +0000

    Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:practice_one/Pages/LoginPage.dart';
 import 'package:practice_one/main_page.dart';
 
 void main() {
@@ -8,6 +9,15 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      // home: HomePage(),
+      themeMode: ThemeMode.system,
+      // darkTheme: ThemeData(brightness: Brightness.dark),
+      theme: ThemeData(primarySwatch: (Colors.green)),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/Login": (context) => LoginPage(),
+      },
+    );
   }
 }
